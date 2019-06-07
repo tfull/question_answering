9ffbc5cb277f863e7c51b09c2eebb1ebec428616
tfull <tfurukawa.mail@gmail.com>
2019-06-07 23:17:59 +0900

    split, mysqlclient

--- src/Split.py
+++ src/Split.py
@@ -1,5 +1,8 @@
 import sys
 import re
+import os
+
+import Config
 
 RE_START = re.compile(r"<page>")
 RE_END = re.compile(r"</page>")
@@ -7,30 +10,48 @@ RE_END = re.compile(r"</page>")
 def write(body, path):
     with open(path, "w") as f:
         f.write("<wikipedia>\n")
+
         for line in body:
             f.write(line)
+
         f.write("</wikipedia>\n")
 
-def main(fname, dirname):
+def main():
+    fname = Config.get("workspace") + "/" + Config.get("wikipedia")
+    dirname = Config.get("workspace") + "/" + Config.get("resource")
+
     flag = False
     body = []
     item_n = 0
     file_id = 1
+
     with open(fname, "r") as f:
         for line in f:
             if RE_START.search(line):
                 flag = True
+
             if flag:
                 body.append(line)
+
             if RE_END.search(line):
                 flag = False
                 item_n += 1
-                if item_n >= 10000:
+
+                if item_n % 10000 == 0:
                     write(body, "{0}/{1:04d}.xml".format(dirname, file_id))
-                    print("output: {0:04d}.xml".format(file_id))
+                    sys.stdout.write("\033[2K\033[G{0:04d}.xml".format(file_id))
+                    sys.stdout.flush()
                     body = []
-                    item_n = 0
                     file_id += 1
 
+    if len(body) > 0:
+        write(body, "{0}/{1:04d}.xml".format(dirname, file_id))
+        sys.stdout.write("\033[2K\033[G{0:04d}.xml".format(file_id))
+        sys.stdout.flush()
+    else:
+        file_id -= 1
+
+    sys.stdout.write("\033[2K\033[GCompleted: {0} items in {1} files\n".format(item_n, file_id))
+
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main()
