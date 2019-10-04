9fa4841d83c8a1c6f6ef59b89a52aed25d0b157a
tfull <tfurukawa.mail@gmail.com>
2019-10-04 23:45:10 +0900

    remove old files

--- erica/base.py
+++
@@ -1,9 +0,0 @@
-from . import logger as Logger
-from . import database as Database
-
-def initialize():
-    Logger.initialize()
-    Database.initialize()
-
-def finalize():
-    Database.finalize()
